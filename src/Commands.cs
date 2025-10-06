f825b871d44c3c14bda62700d218b994273abfc3
Scorpion17471 <171501455+Scorpion17471@users.noreply.github.com>
2025-10-05 20:11:01 -0400

    added echo function + Commands file to store all commands in new file for readability

---
+++ src/Commands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Commands
+    {
+        // Echo - Return everything in input string after "echo "
+        public static void Echo(string input)
+        {
+            Console.WriteLine(input[5..]);
+            return;
+        }
+    }
+}
--- src/main.cs
+++ src/main.cs
@@ -1,20 +1,37 @@
 using System.Data;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 
-while (true)
+namespace src
 {
-    // Uncomment this line to pass the first stage
-    Console.Write("$ ");
-
-    // Wait for user input
-    String? input = Console.ReadLine()?.Trim();
-
-    if (input.ToLower().ToLower()[..4] == "exit")
+    public class MainClass
     {
-        return Convert.ToInt32(input[5..]);
-    }
+        public static int Main(string[] args)
+        {
+            while (true)
+            {
+                // Uncomment this line to pass the first stage
+                Console.Write("$ ");
 
-    // Return invalid command message
-    Console.WriteLine($"{input}: command not found");
+                // Wait for user input
+                String? input = Console.ReadLine()?.Trim();
+
+                // Check for exit command (Exit - Quits shell with given exit code)
+                if (!String.IsNullOrEmpty(input) && input.ToLower()[..4] == "exit")
+                {
+                    return Convert.ToInt32(input[5..]);
+                }
+
+                // Echo Command
+                else if (!String.IsNullOrEmpty(input) && input.ToLower().ToLower()[..4] == "echo")
+                {
+                    Commands.Echo(input);
+                }
+
+                // Return invalid command message
+                Console.WriteLine($"{input}: command not found");
+            }
+        }
+    }
 }

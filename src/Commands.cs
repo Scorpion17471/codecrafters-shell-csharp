41ae7832f9c0e96d2c1d91a69ed95d02240a7cc9
Scorpion17471 <171501455+Scorpion17471@users.noreply.github.com>
2025-10-10 19:35:10 -0400

    created initial path iteration for finding functions

--- src/Commands.cs
+++ src/Commands.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,14 +23,32 @@ namespace src
         // Type - Return type associated with command (built-in vs unrecognized)
         public static string Type(string command)
         {
+            // Check if command is in list of built-in commands
             if (commands.Contains(command))
             {
                 return $"{command} is a shell builtin";
             }
-            else
+
+            // Get Paths and iterate through them to find command
+            string[] paths = Environment.GetEnvironmentVariable("PATH").Split(Path.PathSeparator);
+            foreach (var path in paths)
             {
-                return $"{command}: not found";
+                string fullPath = Path.Combine(path, command);
+                if (File.Exists(fullPath))
+                {
+                    // Check unix file permissions to see if executable
+                    UnixFileMode info = File.GetUnixFileMode(fullPath);
+
+                    if (info.ToString().Contains("execute", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Return if found and executable
+                        return $"{command} is {fullPath}";
+                    }
+                }
             }
+
+            // Default not found message
+            return $"{command}: not found";
         }
     }
 }

f5cb1185690d3a4b18263175c058467a4cf3f2e3
Scorpion17471 <171501455+Scorpion17471@users.noreply.github.com>
2025-10-12 14:13:10 -0400

    separated parser into new class, with command extraction in main, tweaked parser (WIP)

---
+++ src/Parser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public static class Parser
+    {
+        public static List<String> InputParser(string input)
+        {
+            // If input is null or empty, return list with empty string
+            if (String.IsNullOrEmpty(input)) return [""];
+
+            // Setup for argument parsing
+            List<String> output = [];
+            int slow = 0, fast = 0;
+            char delimiter = ' ';
+
+            // Parse arguments, respecting single quotes
+            while (fast < input.Length)
+            {
+                // if we have hit the end of a token, add it to output
+                if (char.Equals(input[fast], delimiter))
+                {
+                    output.Add(input[slow..fast].Trim());
+                    slow = fast + 1;
+                    // if we closed a quote token, reset delimiter to space
+                    delimiter = delimiter == '\'' ? ' ' : delimiter;
+                }
+                // if we have hit a single quote, add arg that we had, change delimiter and move slow pointer
+                else if (char.Equals(input[fast], '\''))
+                {
+                    output.Add(input[slow..fast].Trim());
+                    slow = fast + 1;
+                    delimiter = '\'';
+                }
+                // move fast pointer forward
+                fast++;
+            }
+
+            return output;
+        }
+    }
+}
--- src/main.cs
+++ src/main.cs
@@ -17,77 +17,46 @@ namespace src
                 // Wait for user input
                 String? input = Console.ReadLine()?.Trim();
 
-                // Setup for argument parsing
-                String command = "";
-                List<String> arguments = [];
-                int slow = 0, fast = 0;
-                char delimiter = ' ';
+                List<String> arguments = Parser.InputParser(input ?? String.Empty);
+                String command = arguments[0];
+                arguments.RemoveAt(0);
 
-                // Parse arguments, respecting single quotes
-                while (fast < input.Length)
+                // Check for exit command (Exit - Quits shell with given exit code)
+                if (!String.IsNullOrEmpty(input) && command.Equals("exit", StringComparison.OrdinalIgnoreCase))
                 {
-                    // If we reach a single quote and slow and fast are not both single quotes
-                    // set slow to fast and change delimiter to single quote
-                    if (delimiter == ' ' && input[fast] == '\'')
-                    {
-                        delimiter = '\'';
-                        slow = fast;
-                    }
-                    // If we reach a single quote and slow and fast are both single quotes
-                    // add arg/command and reset delimiter to space
-                    else if (delimiter == '\'' && input[fast] == input[slow])
-                    {
-                        if (slow == 0) command = input[slow..fast];
-                        else arguments.Add(input[(slow + 1)..fast]);
-                        delimiter = ' ';
-                        slow = fast;
-                    }
-                    // If we reach a space and delimiter is space, add arg/command
-                    else if (delimiter == ' ' && input[fast] == delimiter)
-                    {
-                        if (slow == 0) command = input[slow..fast].Trim();
-                        else arguments.Add(input[(slow + 1)..fast].Trim());
-                        slow = fast;
-                    }
-                    fast++;
+                    return Convert.ToInt32(arguments[0]);
                 }
 
-                    // Check for exit command (Exit - Quits shell with given exit code)
-                    if (!String.IsNullOrEmpty(input) && command.Equals("exit", StringComparison.OrdinalIgnoreCase))
-                    {
-                        return Convert.ToInt32(arguments[0]);
-                    }
+                // Echo Command
+                else if (!String.IsNullOrEmpty(input) && command.Equals("echo", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.Write(Commands.Echo(arguments) + '\n');
+                }
 
-                    // Echo Command
-                    else if (!String.IsNullOrEmpty(input) && command.Equals("echo", StringComparison.OrdinalIgnoreCase))
-                    {
-                        Console.Write(Commands.Echo(arguments) + '\n');
-                    }
+                // Type Command
+                else if (!String.IsNullOrEmpty(input) && command.Equals("type", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.Write(Commands.Type(arguments) + '\n');
+                }
 
-                    // Type Command
-                    else if (!String.IsNullOrEmpty(input) && command.Equals("type", StringComparison.OrdinalIgnoreCase))
-                    {
-                        Console.Write(Commands.Type(arguments) + '\n');
-                    }
+                // PWD Command
+                else if (!String.IsNullOrEmpty(input) && command.Equals("pwd", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.Write(Commands.PWD() + '\n');
+                }
 
-                    // PWD Command
-                    else if (!String.IsNullOrEmpty(input) && command.Equals("pwd", StringComparison.OrdinalIgnoreCase))
-                    {
-                        Console.Write(Commands.PWD() + '\n');
-                    }
+                // CD Command
+                else if (!String.IsNullOrEmpty(input) && command.Equals("cd", StringComparison.OrdinalIgnoreCase))
+                {
+                    Commands.CD(arguments);
+                }
 
-                    // CD Command
-                    else if (!String.IsNullOrEmpty(input) && command.Equals("cd", StringComparison.OrdinalIgnoreCase))
-                    {
-                        Commands.CD(arguments);
-                    }
-
-                    // Unknown Command
-                    else
-                    {
-                        // Try command
-                        Commands.TestCommand(command, arguments);
-                    }
+                // Unknown Command
+                else
+                {
+                    // Try command
+                    Commands.TestCommand(command, arguments);
+                }
             }
         }
     }
